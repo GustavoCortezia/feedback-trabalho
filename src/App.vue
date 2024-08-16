bf42746ca2b72a851eca3ba1bdf95d7990384d82
Gustavo Cortezia <gus.cortezia@gmail.com>
2024-08-16 17:15:25 -0300

    refatoracao

--- src/App.vue
+++ src/App.vue
@@ -4,49 +4,124 @@
 
     <v-container class="background">
 
-      <div class="imagem mt-15">
-        <img class="mt-15" src="./assets/fundo.jpg" alt="">
-      </div>
+<h1>Feedback Grupos</h1>
 
       <div class="cards">
-        <v-card
-          class="ma-10"
-          width="500px"
-          subtitle="- cortês - cooperatividade - liderança"
-          title="Gustavo Cortezia"
-          link
-        ></v-card>
+        <v-row class="row">
 
-        <v-card
-          class="ma-10"
-          width="500px"
-          subtitle="- humildade
-    - empenho
-    - serenidade"
-          title="Bernardo Dartora"
-          link
-        ></v-card>
+          <v-card
+            class="card mx-6"
+            max-width="344"
+            width="240px"
+          >
+            <v-img
+              height="200px"
+              src="./assets/Captura de tela 2024-06-04 202722.png"
+              cover
+            ></v-img>
 
-        <v-card
-          class="ma-10"
-          width="500px"
-          subtitle="- bom ouvinte
-    - seriedade
-    - trabalho em equipe"
-          title="Alexsandro da Rocha"
-          link
-        ></v-card>
+            <v-card-title>
+              Gustavo Cortezia
+            </v-card-title>
 
-        <v-card
-          class="ma-10"
-          width="500px"
-          subtitle="- criativo
-    - caridoso
-    - comunicação assertiva"
-          title="Daniel Gheno"
-          link
-        ></v-card>
-      </div>
+
+            <v-expand-transition>
+              <div>
+                <v-divider></v-divider>
+
+                <v-card-text>Cortês</v-card-text>
+                <v-card-text>Cooperatividade</v-card-text>
+                <v-card-text>Liderança</v-card-text>
+
+              </div>
+            </v-expand-transition>
+          </v-card>
+
+
+          <v-card
+            class="card mx-6"
+            max-width="344"
+            width="240px"
+          >
+            <v-img
+              height="200px"
+              src="./assets/image-bernardo.png"
+              cover
+            ></v-img>
+
+            <v-card-title>
+              Bernardo Dartora
+            </v-card-title>
+
+
+            <v-expand-transition>
+              <div>
+                <v-divider></v-divider>
+
+                <v-card-text>Humildade</v-card-text>
+                <v-card-text>Empenho</v-card-text>
+                <v-card-text>Serenidade</v-card-text>
+
+              </div>
+            </v-expand-transition>
+          </v-card>
+
+                <v-card
+            class="card mx-6"
+            max-width="344"
+            width="240px"
+          >
+            <v-img
+              height="200px"
+              src="./assets/379848077_1071135470884813_6306204106673045828_n.jpg"
+              cover
+            ></v-img>
+
+            <v-card-title>
+              Alexsandro da Rocha
+            </v-card-title>
+
+
+            <v-expand-transition>
+              <div>
+                <v-divider></v-divider>
+
+                <v-card-text>Bom ouvinte</v-card-text>
+                <v-card-text>Seriedade</v-card-text>
+                <v-card-text>Trabalho em Equipe</v-card-text>
+
+              </div>
+            </v-expand-transition>
+          </v-card>
+                <v-card
+            class="card mx-6"
+            max-width="344"
+            width="240px"
+          >
+            <v-img
+              height="200px"
+              src="./assets/daniel-foto.jpg"
+              cover
+            ></v-img>
+
+            <v-card-title>
+              Daniel Gheno
+            </v-card-title>
+
+
+            <v-expand-transition>
+              <div>
+                <v-divider></v-divider>
+
+                <v-card-text>Criativo</v-card-text>
+                <v-card-text>Caridoso</v-card-text>
+                <v-card-text>Communicação Assertiva</v-card-text>
+
+              </div>
+            </v-expand-transition>
+          </v-card>
+        </v-row>
+              </div>
 
     </v-container>
   </div>
